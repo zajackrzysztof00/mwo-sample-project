6cd04937657499286575548a089bc8323d112a35
student <dsad@dsad.j>
2025-05-10 15:11:38 +0200

    Added class to find first and last pitstop lap number

---
+++ src/main/java/LapNumberFinder.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class LapNumberFinder {
+    public static int findFirstPitStopLapNumber(List<PitStop> pitStops) {
+        int lapNumber = Integer.MAX_VALUE;
+        for (PitStop pitStop : pitStops) {
+            lapNumber = Math.min(lapNumber, pitStop.getLap());
+        }
+        return lapNumber;
+    }
+
+    public static int findLastPitStopLapNumber(List<PitStop> pitStops) {
+        int lapNumber = 0;
+        for (PitStop pitStop : pitStops) {
+            lapNumber = Math.max(lapNumber, pitStop.getLap());
+        }
+        return lapNumber;
+    }
+}
